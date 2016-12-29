0609bfe1f6bada1bb4176336dfef2b887ebbe6ff
Brandon Olin <brandon@devblackops.io>
2016-12-28 21:02:34 -0800

    Accept imput from pipeline

--- PSHealthZ/Public/Get-HealthZListener.ps1
+++ PSHealthZ/Public/Get-HealthZListener.ps1
@@ -11,7 +11,6 @@ function Get-HealthZListener {
         Get-HealthZListener
 
         Gets all currently running PSHealthZ listeners.
-
     .EXAMPLE
         Get-HealthZListener -Id 101
 
@@ -20,22 +19,45 @@ function Get-HealthZListener {
     [OutputType([pscustomobject])]
     [cmdletbinding()]
     param(
-        [int]$Id
+        [parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [int[]]$Id = @()
     )
 
-    $script:httpListeners.GetEnumerator() | ForEach-Object {
-        [pscustomobject]@{
-            Id = $_.Name
-            State = (Get-Job -Id $_.Name).State
-            Uri = $_.Value.Uri
-            Port = $_.Value.port
-            Path = $_.Value.path
-            Auth = $_.Value.auth
-            SSL = $_.Value.ssl
-            CertificateThumbprint = $_.Value.certificateThumbprint
-            Token = $_.Value.token
-            Log = $_.Value.log
-            InstanceId = $_.Value.instanceId
+    process {
+        if ($Id.Count -gt 0) {
+            foreach ($item in $Id) {
+                if ($l = $script:httpListeners.$item) {
+                    [pscustomobject][ordered]@{
+                        Id = $l.jobId
+                        State = (Get-Job -Id $l.jobId).State
+                        Uri = $l.Uri
+                        Port = $l.port
+                        Path = $l.path
+                        Auth = $l.auth
+                        SSL = $l.ssl
+                        CertificateThumbprint = $l.certificateThumbprint
+                        Token = $l.token
+                        Log = $l.log
+                        InstanceId = $l.instanceId
+                    }
+                }
+            }
+        } else {
+            $script:httpListeners.GetEnumerator() | ForEach-Object {
+                [pscustomobject][ordered]@{
+                    Id = $_.Value.jobId
+                    State = (Get-Job -Id $_.Value.jobId).State
+                    Uri = $_.Value.Uri
+                    Port = $_.Value.port
+                    Path = $_.Value.path
+                    Auth = $_.Value.auth
+                    SSL = $_.Value.ssl
+                    CertificateThumbprint = $_.Value.certificateThumbprint
+                    Token = $_.Value.token
+                    Log = $_.Value.log
+                    InstanceId = $_.Value.instanceId
+                }
+            }
         }
     }
 }
